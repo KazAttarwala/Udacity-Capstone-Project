3e513c30710f3691f5082fb45b3ccd7da36ec614
KazAttarwala <52388168+KazAttarwala@users.noreply.github.com>
2021-08-11 22:43:50 -0500

    removed unnecessary files and modified Procfile to add migrations to app

--- backend/setup.sh
+++
@@ -1,4 +0,0 @@
-export DATABASE_URL='postgres://mdaozsjziktvsh:c63fbbfe69ebd5b84c92e1eeab27f48b6ff4c2ce77dc0e19f9f888a90cf90739@ec2-3-218-149-60.compute-1.amazonaws.com:5432/dcv1g8b33mlihi'
-export APP_AUTH0_DOMAIN='udacity-coffee-full-stack.us.auth0.com'
-export ALGORITHMS='RS256'
-export APP_API_AUDIENCE='https://localhost:5000'
